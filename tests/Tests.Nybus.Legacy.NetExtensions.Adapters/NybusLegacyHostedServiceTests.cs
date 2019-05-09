554e0037f8b9b05e16bf32f57d8768d465b142b7
Renato Golia <renato.golia@outlook.com>
2019-05-09 17:32:19 +0200

    Initial commits (#2)
    Adapters for
    - `IHostedService`
    - `IServiceProvider` and `IScope`
    - `ILoggerFactory` and `ILogger`
    
    Extension methods to enrich `IServiceCollection`

---
+++ tests/Tests.Nybus.Legacy.NetExtensions.Adapters/NybusLegacyHostedServiceTests.cs
@@ -0,0 +1,35 @@
+﻿using System.Threading.Tasks;
+using AutoFixture.Idioms;
+using AutoFixture.NUnit3;
+using Moq;
+using NUnit.Framework;
+using Nybus;
+
+namespace Tests
+{
+    [TestFixture]
+    public class NybusLegacyHostedServiceTests
+    {
+        [Test, AutoMoqData]
+        public void Constructor_is_guarded(GuardClauseAssertion assertion)
+        {
+            assertion.Verify(typeof(NybusLegacyHostedService).GetConstructors());
+        }
+
+        [Test, AutoMoqData]
+        public async Task StartAsync_starts_bus([Frozen] IBus bus, NybusLegacyHostedService sut)
+        {
+            await sut.StartAsync(default);
+
+            Mock.Get(bus).Verify(p => p.Start());
+        }
+
+        [Test, AutoMoqData]
+        public async Task StopAsync_stops_bus([Frozen] IBus bus, NybusLegacyHostedService sut)
+        {
+            await sut.StopAsync(default);
+
+            Mock.Get(bus).Verify(p => p.Stop());
+        }
+    }
+}
