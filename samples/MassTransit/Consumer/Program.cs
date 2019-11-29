8a5c6d156d1c578c0ac7dca41c517d202e1cf488
Renato Golia <renato.golia@educations.com>
2019-11-29 18:33:48 +0100

    Added samples for MassTransit

---
+++ samples/MassTransit/Consumer/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Messages;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Nybus;
+using Nybus.Configuration;
+
+namespace Consumer
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var configurationBuilder = new ConfigurationBuilder();
+
+            configurationBuilder.AddInMemoryCollection(new Dictionary<string, string>
+            {
+                ["ConnectionStrings:ServiceBus"] = "host=rabbitmq://localhost;username=guest;password=guest"
+            });
+
+            var configuration = configurationBuilder.Build();
+
+            var services = new ServiceCollection();
+
+            services.AddSingleton<IConfiguration>(configuration);
+
+            services.AddLogging(logging => logging.AddConsole().SetMinimumLevel(LogLevel.Trace));
+
+            services.AddNybusLegacyWithMassTransit(new NybusLegacyConfiguration
+            {
+                ConnectionStringName = "ServiceBus",
+                SubscriptionsConfigurator = builder =>
+                {
+                    builder.SubscribeToCommand<ProduceItem>();
+                },
+                MassTransitConfigurator = options =>
+                {
+                    options.CommandQueueStrategy = new PrefixedTemporaryQueueStrategy("Queue");
+
+                    options.ServiceBusFactory = new RabbitMqServiceBusFactory(Environment.ProcessorCount);
+                }
+            });
+
+            services.RegisterCommandHandler<ProduceItem, ProduceItemCommandHandler>();
+
+            var serviceProvider = services.BuildServiceProvider();
+
+            var hostedService = serviceProvider.GetRequiredService<IHostedService>();
+
+            await hostedService.StartAsync(default);
+
+            Console.WriteLine("Press ENTER to close");
+            Console.ReadLine();
+
+            await hostedService.StopAsync(default);
+        }
+    }
+
+    public class ProduceItemCommandHandler : ICommandHandler<ProduceItem>
+    {
+        private readonly IBus _bus;
+        private readonly ILogger<ProduceItemCommandHandler> _logger;
+
+        public ProduceItemCommandHandler(IBus bus, ILogger<ProduceItemCommandHandler> logger)
+        {
+            _bus = bus ?? throw new ArgumentNullException(nameof(bus));
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
+        public async Task Handle(CommandContext<ProduceItem> commandMessage)
+        {
+            await Task.Delay(TimeSpan.FromMilliseconds(10));
+
+            await _bus.RaiseEvent(new ItemProduced
+            {
+                ItemId = commandMessage.Message.ItemId,
+                Quantity = commandMessage.Message.Quantity
+            });
+        }
+    }
+}
---
+++ samples/MassTransit/Producer/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Messages;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Nybus;
+using Nybus.Configuration;
+
+namespace Producer
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var configurationBuilder = new ConfigurationBuilder();
+
+            configurationBuilder.AddInMemoryCollection(new Dictionary<string, string>
+            {
+                ["ConnectionStrings:ServiceBus"] = "host=rabbitmq://localhost;username=guest;password=guest"
+            });
+            
+            var configuration = configurationBuilder.Build();
+
+            var services = new ServiceCollection();
+            
+            services.AddSingleton<IConfiguration>(configuration);
+            
+            services.AddLogging(logging => logging.AddConsole().SetMinimumLevel(LogLevel.Trace));
+
+
+            services.AddNybusLegacyWithMassTransit(new NybusLegacyConfiguration
+            {
+                ConnectionStringName = "ServiceBus",
+                SubscriptionsConfigurator = builder =>
+                {
+                    builder.SubscribeToEvent<ItemProduced>(async e =>
+                    {
+                        await Console.Out.WriteLineAsync($"Received: {e.Message.Quantity} of {e.Message.ItemId} were produced!");
+                    });
+                },
+                MassTransitConfigurator = options =>
+                {
+                    options.EventQueueStrategy = new TemporaryQueueStrategy();
+
+                    options.ServiceBusFactory = new RabbitMqServiceBusFactory(Environment.ProcessorCount);
+                }
+            });
+
+            var serviceProvider = services.BuildServiceProvider();
+
+            var hostedService = serviceProvider.GetRequiredService<IHostedService>();
+
+            await hostedService.StartAsync(default);
+
+            var bus = serviceProvider.GetRequiredService<IBus>();
+
+            var correlationId = Guid.NewGuid();
+
+            for (var i = 0; i < 100; i++)
+            {
+                await bus.InvokeCommand(new ProduceItem { ItemId = Guid.NewGuid(), Quantity = 1f }, correlationId);
+            }
+
+            await hostedService.StopAsync(default);
+        }
+    }
+}
